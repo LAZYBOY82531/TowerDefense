aac52266d37c76d99e5de7995643778496c9e9af
Home raynor <fallere82531@gmail.com>
2023-10-03 21:40:21 +0900

    fix : 버그 픽스 및 타이틀 이미지 추가

---
+++ Assets/Scripts/UI/GameEndUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEndUI : MonoBehaviour
+{
+    public void GameEnd()
+    {
+        Application.Quit();
+    }
+}
